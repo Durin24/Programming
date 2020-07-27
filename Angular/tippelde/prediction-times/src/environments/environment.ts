5821bcdd58f83d98d24d44a773b4082d8ea78d67
Peter Guber <petr.guber@gmail.com>
2020-07-27 23:50:55 +0200

    firebase imports

--- Angular/tippelde/prediction-times/src/environments/environment.ts
+++ Angular/tippelde/prediction-times/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAUfhqXj3eKVTjfx6JaR_BFi7kE1LeBo60',
+    authDomain: 'prediction-times.firebaseapp.com',
+    databaseURL: 'https://prediction-times.firebaseio.com',
+    projectId: 'prediction-times',
+    storageBucket: 'prediction-times.appspot.com',
+    messagingSenderId: '343006750532',
+    appId: '1:343006750532:web:7575e66753fd3e6d2b5911',
+    measurementId: 'G-XZ66782HMD'
+  }
 };
 
 /*
