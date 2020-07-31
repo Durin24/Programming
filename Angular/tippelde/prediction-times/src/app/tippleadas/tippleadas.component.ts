50a66be4e5ea8699ccc5914015aa1073bc2f9c00
Peter Guber <petr.guber@gmail.com>
2020-07-31 03:19:08 +0200

    javitas

--- Angular/tippelde/prediction-times/src/app/tippleadas/tippleadas.component.ts
+++ Angular/tippelde/prediction-times/src/app/tippleadas/tippleadas.component.ts
@@ -16,21 +16,21 @@ export class TippleadasComponent implements OnInit {
   constructor(private db: AngularFireDatabase, private authService: AuthService) { }
 
   ngOnInit(): void {
-    var firebaseConfig = {
-      apiKey: "AIzaSyAUfhqXj3eKVTjfx6JaR_BFi7kE1LeBo60",
-      authDomain: "prediction-times.firebaseapp.com",
-      databaseURL: "https://prediction-times.firebaseio.com",
-      projectId: "prediction-times",
-      storageBucket: "prediction-times.appspot.com",
-      messagingSenderId: "343006750532",
-      appId: "1:343006750532:web:7575e66753fd3e6d2b5911",
-      measurementId: "G-XZ66782HMD"
+    const firebaseConfig = {
+      apiKey: 'AIzaSyAUfhqXj3eKVTjfx6JaR_BFi7kE1LeBo60',
+      authDomain: 'prediction-times.firebaseapp.com',
+      databaseURL: 'https://prediction-times.firebaseio.com',
+      projectId: 'prediction-times',
+      storageBucket: 'prediction-times.appspot.com',
+      messagingSenderId: '343006750532',
+      appId: '1:343006750532:web:7575e66753fd3e6d2b5911',
+      measurementId: 'G-XZ66782HMD'
     };
-    var user = this.authService.user;
+    const user = this.authService.user;
     // Initialize Firebase
-    //firebase.initializeApp(firebaseConfig);
-    var dbRef = this.db.list('/Users').valueChanges();
-    dbRef.subscribe( valueOfItems => {console.log(valueOfItems)});
+    /* firebase.initializeApp(firebaseConfig); */
+    const dbRef = this.db.list('/Users').valueChanges();
+    dbRef.subscribe( valueOfItems => {console.log(valueOfItems); });
 
   }
 
